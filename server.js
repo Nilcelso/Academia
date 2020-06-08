2906e631cd4699257faa31f13f5c1ad881ee4e09
Nilcelso Sobrinho <nilcelsop@gmail.com>
2020-06-08 09:40:26 -0300

    Criando o servidor

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require("./routes")
+
+const server = express()
+
+server.use(express.static('public'))
+server.use(routes)
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true, // boolea verdadeiro ou false true ou false
+})
+
+
+server.listen(5000, function () {
+    console.log("server is running")
+})
