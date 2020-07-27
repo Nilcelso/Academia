f36c73f49c43d62a58058d26768f3453141bced6
Nilcelso Sobrinho <nilcelsop@gmail.com>
2020-07-27 15:32:40 -0300

    estilizando formulario

--- routes.js
+++ routes.js
@@ -14,7 +14,8 @@ routes.get('/instructors/create', function (req, res) {
 })
 
 routes.post("/instructors", function(req, res) {
-    return res.send("recebido")
+
+    return res.send(req.body)
 })
 
 routes.get('/members', function (req, res) {
